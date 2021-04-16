afe12ffa05b4c305849b82497b312dfe95658baa
Alexander Sarkisov <sarkisov.dev@gmail.com>
2021-04-16 16:40:56 +0300

    set up Link page

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const LinkCard = ({ link }) => {
+  return (
+    <>
+      <h2>Link</h2>
+      <p>
+        To link:{' '}
+        <a href={link.to} target='_blank' rel='noopener noreferrer'>
+          {link.to}
+        </a>
+      </p>
+      <p>
+        From link:{' '}
+        <a href={link.from} target='_blank' rel='noopener noreferrer'>
+          {link.from}
+        </a>
+      </p>
+      <p>
+        Click count: <strong>{link.clicks}</strong>
+      </p>
+      <p>
+        Creation date: <strong>{new Date(link.date).toLocaleDateString('ru-RU')}</strong>
+      </p>
+    </>
+  )
+}
